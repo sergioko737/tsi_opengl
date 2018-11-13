b0df1d174b38c4dcb8c7978771dc9058a29ef7b3
sergioko737 <s29585476@gmail.com>
2018-11-14 01:17:45 +0200

    DN LR 2_1

---
+++ DN_LR2/HelloSolution/HelloVB/HelloVB.vb
@@ -0,0 +1,7 @@
+﻿Public Class HelloVB
+    Inherits HelloCPP.HelloCPP
+    Public Overrides Sub Hello()
+        MyBase.Hello()
+        System.Console.WriteLine("Hello from Visual Basic")
+    End Sub
+End Class
