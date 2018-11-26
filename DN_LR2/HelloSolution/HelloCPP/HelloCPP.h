c86dbc144eee28f0f2013d3a529289917bed3cc8
sergioko737 <s29585476@gmail.com>
2018-11-26 16:16:29 +0200

    DN lr 2 3 4

--- DN_LR2/HelloSolution/HelloCPP/HelloCPP.h
+++ DN_LR2/HelloSolution/HelloCPP/HelloCPP.h
@@ -8,6 +8,10 @@ namespace HelloCPP {
 
 	public ref class HelloCPP
 	{
+		HelloCPP(int x){
+			Console::WriteLine("Hello from C++++++");
+		}
+
 		public:virtual void Hello()
 		{
 			Console::WriteLine("Hello from C++");
--- DN_LR2/HelloSolution/HelloVB/HelloVB.vb
+++ DN_LR2/HelloSolution/HelloVB/HelloVB.vb
@@ -1,7 +1,7 @@
 ﻿Public Class HelloVB
     Inherits HelloCPP.HelloCPP
     Public Overrides Sub Hello()
-        MyBase.Hello()
+        MyBase.Hello(3)
         System.Console.WriteLine("Hello from Visual Basic")
     End Sub
 End Class
