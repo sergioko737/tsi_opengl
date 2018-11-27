b83af750b419345584f48b36d8035b75414e397e
sergioko737 <s29585476@gmail.com>
2018-11-28 01:55:58 +0200

    dn lr4

--- DN_LR4/PersonToFile/PersonToFile/Person.cs
+++ DN_LR4/PersonToFile/PersonToFile/Person.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace PersonToFile
 {
+    [Serializable]
     class Person
     {
+        private String name;
+        private String persCode;
+        private int age;
+
+        public String Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public String PersCode
+        {
+            get
+            {
+                return persCode;
+            }
+            set
+            {
+                persCode = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
     }
 }
--- DN_LR4/PersonToFile/PersonToFile/Program.cs
+++ DN_LR4/PersonToFile/PersonToFile/Program.cs
@@ -10,6 +10,7 @@ namespace PersonToFile
     {
         static void Main(string[] args)
         {
+            DirectoryInfo
         }
     }
 }
