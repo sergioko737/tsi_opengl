981da12f0c20c72a90a3e19ea6d71b0daea891c9
sergioko737 <s29585476@gmail.com>
2018-12-12 03:16:35 +0200

    dnLr4 some tasks are ready

--- DN_LR4/PersonToFile/Person.cs
+++ DN_LR4/PersonToFile/Person.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace PersonToFile
 {
+    [Serializable]
     class Person
     {
+        public String name { get; set; }
+        public String persCode { get; set; }
+        public byte age { get; set; }
     }
 }
