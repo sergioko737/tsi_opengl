b1a7affa3f68027ba1a1091d67bcf473bf8586b6
sergioko737 <s29585476@gmail.com>
2018-12-19 12:39:08 +0200

    DN lr5

---
+++ DN_LR5/LR5/LR5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program will count phone call duration.");
+            Console.WriteLine("You will need to enter phone call start/end times as 3 numbers(hours, minutes, seconds)");
+            Console.WriteLine("***Constraint: call must be inside one daylight");
+        }
+    }
+}
